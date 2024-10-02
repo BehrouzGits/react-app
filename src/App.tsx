5a71d57756496b4f570ba074eb29961e294441df
Behrouz <b.ayanpour@gmail.com>
2024-10-02 18:49:01 +0330

    onSelectedItem 1403/07/11

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["New York", "San  francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities"/>
+      <ListGroup items={items} heading="Cities" onSelectedItem={handleSelectItem} />
     </div>
   );
 }
